1e5e811a92b3a8f7f076006674791bd206f9b684
rarnaout <rarnaout@gmail.com>
2020-06-04 17:25:15 -0400

    Restoration of deleted __init__.py

--- ct2vl/__init__.py
+++ ct2vl/__init__.py
@@ -1,9 +1,7 @@
 import numpy as np
 
-print("""Please cite:
-
-Arnaout et al. SARS-CoV-2 Testing: The Limit of Detection Matters—The Case for Benchmarking. bioRxiv, In press.
-""")
+print("""Please cite: 
+Arnaout et al. SARS-CoV-2 Testing: The Limit of Detection Matters—The Case for Benchmarking. bioRxiv, In press.""")
 
 m = -0.0283 # slope of line relating efficiency to cycle no (R2=0.82)
 b = m*1.41 + (1.379 + 1) # = rho_0 = efficiency + 1
@@ -11,7 +9,6 @@ vL = 100 # viral load at LoD, in copies/mL
 CtL = 26.06 # Ct value at LoD (result of BIDMC validation)
 
 def ct2vl(Ct):
-	#
 	log_v = np.log(vL) + (CtL-1+b/m)*np.log(m*(CtL-1)+b) - (Ct-1+b/m)*np.log(m*(Ct-1)+b)  + Ct-CtL
 	return np.exp( log_v )
 
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md') as readme_file:
 
 setuptools.setup(
 	name="ct2vl",
-	version="0.0.3",
+	version="0.0.4",
 	author= "Ramy Arnaout",
 	author_email="rarnaout@bidmc.harvard.edu",
 	description="Convert Ct values to viral loads for the Abbott m2000 SARS-CoV-2 RT-PCR test",
@@ -14,7 +14,7 @@ setuptools.setup(
 	url ="https://github.org/rarnaout/ct2vl",
 	packages=setuptools.find_packages(),
 	classifiers = [
-		"Programming Language :: Python :: 3", # took the examples straight from python.org so may need to adjust these details 
+		"Programming Language :: Python :: 3",
 		"License :: OSI Approved :: MIT License",
 		"Operating System :: OS Independent",
 	],
