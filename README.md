adbbfde2c3a82c95875e0b7563da26c42aec4e43
rarnaout <rarnaout@gmail.com>
2020-06-04 16:25:25 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+ct2vl.py is for use with the Abbott SARS-CoV-2 RT-PCR (Covid-19) test on the Abbott m2000 system as validated at Beth Israel Deaconess Medical Center. If you want to use this at some other site or on/for some other system, you will want to be sure that efficiency falls linearly with Ct, and measure the appropriate constants. (If you want to customize further, please see the derivation of the math in the citation below.)
+
+If you use this conversion, please cite:
+
+Arnaout et al. SARS-CoV-2 Testing: The Limit of Detection Matters—The Case for Benchmarking. bioRxiv, In press.
---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools 
+
+with open('README.md') as readme_file:
+	README = readme_file.read()
+
+setuptools.setup(
+	name="ct2vl",
+	version="0.0.1",
+	author= "Ramy Arnaout",
+	author_email="rarnaout@bidmc.harvard.edu",
+	description="Convert Ct values to viral loads for the Abbott m2000 SARS-CoV-2 RT-PCR test",
+	long_description=README, 
+	long_description_content_type="text/markdown",
+	url ="https://github.org/rarnaout/ct2vl",
+	packages=setuptools.find_packages(),
+	classifiers = [
+		"Programming Language :: Python :: 3", # took the examples straight from python.org so may need to adjust these details 
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
+	python_requires ='>=3.6',
+)
+
