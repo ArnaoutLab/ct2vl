8658bba528907760ba8410900ecaa25782caf69b
rarnaout <rarnaout@gmail.com>
2020-06-04 16:49:23 -0400

    Updated __init__.py
    Tabs

--- build/lib/ct2vl/__init__.py
+++ build/lib/ct2vl/__init__.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-print(""Please cite:
+print("""Please cite:
 
 Arnaout et al. SARS-CoV-2 Testing: The Limit of Detection Matters—The Case for Benchmarking. bioRxiv, In press.
 """)
@@ -11,9 +11,9 @@ vL = 100 # viral load at LoD, in copies/mL
 CtL = 26.06 # Ct value at LoD (result of BIDMC validation)
 
 def ct2vl(Ct):
-#
-log_v = np.log(vL) + (CtL-1+b/m)*np.log(m*(CtL-1)+b) - (Ct-1+b/m)*np.log(m*(Ct-1)+b)  + Ct-CtL
-return np.exp( log_v )
+	#
+	log_v = np.log(vL) + (CtL-1+b/m)*np.log(m*(CtL-1)+b) - (Ct-1+b/m)*np.log(m*(Ct-1)+b)  + Ct-CtL
+	return np.exp( log_v )
 
 assert(np.round(ct2vl(CtL)*100)/100==vL)
 
